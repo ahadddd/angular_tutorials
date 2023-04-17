8aa8b74eba1027bc97f7209d65c72ae8ed28d4e2
ahadddd <ahad.shehbaz19@gmail.com>
2023-04-17 16:03:37 +0500

    final commit 170423

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Angular!';
+  password = '';
+  correctPass = 'test';
+  color = 'green';
+
+  products = [
+    {
+      sku: 'T-2139',
+      name: 'Bottle',
+      price: 23
+    },
+    {
+      sku: 'T-4267',
+      name: 'Scarf',
+      price: 23
+    },
+  ];
+
+
+
+
+  checkPassword(): boolean {
+    if (this.password == this.correctPass) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  addProduct(){
+    let prod = {
+      sku: 'T-'+Math.floor(Math.random()* 5000).toString(),
+      name: 'Dummy Product',
+      price: Math.floor(Math.random()* 100)
+    }
+    this.products.push(prod);
+  }
+
+  remove(i: number) {
+    this.products.splice(i, 1);
+  }
+
 }
