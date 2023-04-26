6c8785a5ee395e4b160ca528f9a47e443bd9f9a4
ahadddd <ahad.shehbaz19@gmail.com>
2023-04-26 17:41:43 +0500

    new class created in libs

--- src/app/users/users/users.component.ts
+++ src/app/users/users/users.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/libs/data';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
+  usersData: any = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+    let data = new DataService();
+    this.usersData = data.users;
+
+  }
 }
---
+++ src/libs/data.ts
@@ -0,0 +1,20 @@
+export class DataService {
+    users = [
+        {
+            firstName: 'Ahad',
+            secName: 'Shehbaz'
+        },
+        {
+            firstName: 'Hassan',
+            secName: 'Ali'
+        },
+        {
+            firstName: 'Omar',
+            secName: 'Saeed'
+        },
+        {
+            firstName: 'Mustafa',
+            secName: 'Zafar'
+        },
+    ]
+}
